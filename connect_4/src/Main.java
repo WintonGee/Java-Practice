d39befa7067e335d87fc8433a8e51013091d2c13
wintongee <wgee01@calpoly.edu>
2022-11-11 13:07:01 -0800

    Added color

--- connect_4/src/Main.java
+++ connect_4/src/Main.java
@@ -2,23 +2,37 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static int ROWS = 6, COLUMS = 7;
+    public static int ROWS = 6, COLUMNS = 7;
     public static char EMPTY_INDICATOR = '-', FIRST_PLAYER = 'O', SECOND_PLAYER = 'X';
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+
 
     public static void main(String[] args) {
         // Init board
-        char[][] board = new char[ROWS][COLUMS];
+        char[][] board = new char[ROWS][COLUMNS];
         for (int i = 0; i < board.length; i++)
             for (int j = 0; j < board[0].length; j++)
                 board[i][j] = EMPTY_INDICATOR;
 
         Scanner myObj = new Scanner(System.in);
 
+        // Getting the player names
+        System.out.println("Name of first player: ");
+        String firstPlayerName = myObj.nextLine();
+        System.out.println("Name of second player: ");
+        String secondPlayerName = myObj.nextLine();
+
         char player = FIRST_PLAYER;
         while (true) {
             display(board);
 
-            System.out.println("Player " + player + " pick a position: ");
+            String playerName = player == FIRST_PLAYER ? firstPlayerName : secondPlayerName;
+            String color = player == FIRST_PLAYER ? ANSI_RED : ANSI_YELLOW;
+            System.out.println("(" + color + "O" + ANSI_RESET + ") " + playerName + " pick a position: ");
             String input = myObj.nextLine();
 
             if (!isNumeric(input)) {
@@ -32,7 +46,6 @@ public class Main {
                 continue;
             }
 
-            // TODO adjust the display and fix the index checking...
             int positionHeight = getPositionHeight(board, position);
             if (positionHeight == -1) {
                 System.out.println("Please choose a column with an empty slot");
@@ -40,16 +53,18 @@ public class Main {
             }
 
             // Update the board
-            board[position][positionHeight] = player;
+            board[positionHeight][position] = player;
 
             if (isPlayerWon(board, player)) {
-                System.out.println("Game Over: " + player + " Wins!");
-                return;
+                display(board);
+                System.out.println("Game Over: " + playerName + " Wins!");
+                break;
             }
 
             if (isGameTied(board)) {
+                display(board);
                 System.out.println("Game Over: Tied Game!");
-                return;
+                break;
             }
 
             // Swap player's turn
@@ -57,6 +72,8 @@ public class Main {
         }
     }
 
+    // Note: Could also check only the top of the board for more efficiency
+    // Return: If all rows and columns are filled
     public static boolean isGameTied(char[][] board) {
         for (char[] i : board)
             for (char j : i)
@@ -65,13 +82,53 @@ public class Main {
         return true;
     }
 
+    // TODO
+    // Note: The checking algorithms are not the most efficient, but easiest to understand
+    // Condition for winning: 4 connections in a row
+    // Check 1: Vertical
+    // Check 2: Horizontal
+    // Check 3: Diagonal to the left
+    // Check 4: Diagonal to the right
     public static boolean isPlayerWon(char[][] board, char player) {
+        int connections = 0; // Used to keep track of how many connects in a row
+
+        // Check 1: Vertical
+        for (int i = 0; i < COLUMNS; i++) {
+            for (int j = 0; j < ROWS; j++) {
+                if (connections >= 4)
+                    return true;
+                if (board[j][i] == player)
+                    connections++;
+                else
+                    connections = 0;
+            }
+        }
+
+        // Check 2: Horizontal
+        connections = 0;
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLUMNS; j++) {
+                if (connections >= 4)
+                    return true;
+                if (board[i][j] == player)
+                    connections++;
+                else
+                    connections = 0;
+            }
+        }
+
+        // Check 3: Diagonal to the left
+        connections = 0;
+
+        // Check 4: Diagonal to the right
+        connections = 0;
+
         return false;
     }
 
     // Return: If string is a numeric string
     public static boolean isNumeric(String string) {
-        if(string == null || string.equals("")) {
+        if (string == null || string.equals("")) {
             return false;
         }
 
@@ -86,14 +143,14 @@ public class Main {
     // Returns: if the player's desired location is valid.
     // Valid when: pos is within the range 0 - 6, inclusive and space is remaining in the column pos
     public static boolean isPositionWithinRange(int pos) {
-        return pos >= 0 && pos < ROWS;
+        return pos >= 0 && pos < COLUMNS;
     }
 
     // PreCheck: Position is valid
     // Returns: the height of where to place a piece on the board
     public static int getPositionHeight(char[][] board, int pos) {
         for (int height = 0; height < ROWS; height++) {
-            if (board[pos][height] == EMPTY_INDICATOR)
+            if (board[height][pos] == EMPTY_INDICATOR)
                 return height;
         }
 
@@ -101,15 +158,22 @@ public class Main {
     }
 
     public static void display(char[][] board) {
-        System.out.println("-------------");
-        for (char[] i : board) {
-            for (char j : i)
-                System.out.print(j + " ");
+        System.out.println(ANSI_BLUE + "-----------------" + ANSI_RESET);
+
+        for (int i = board.length - 1; i >= 0; i--) {
+            System.out.print(ANSI_BLUE + "| " + ANSI_RESET);
+            for (int j = 0; j < board[0].length; j++)
+                System.out.print(board[i][j] + " ");
+            System.out.print(ANSI_BLUE + "|" + ANSI_RESET);
             System.out.println();
         }
 
-        for (int i = 0; i < COLUMS; i++)
+        System.out.println(ANSI_BLUE + "-----------------" + ANSI_RESET);
+
+        System.out.print(ANSI_BLUE + "| " + ANSI_RESET);
+        for (int i = 0; i < COLUMNS; i++)
             System.out.print(i + " ");
+        System.out.print(ANSI_BLUE + "|" + ANSI_RESET);
         System.out.println();
     }
 
